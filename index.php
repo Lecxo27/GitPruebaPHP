2b7094193cb370ddb2948b6bfd8214d1c39b3ff7
Luis Bohorquez <lecxo14@gmail.com>
2023-09-14 15:51:58 -0400

    Cambios

--- index.php
+++ index.php
@@ -3,16 +3,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/style.css">
+    <link rel="stylesheet" href="./style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+        html, body {
+            height: 100%;
+        }
+
+        .container {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100%;
+        }
+
+        .btn-group {
+            margin-top: 10px;
+        }
+    </style>
     <title>Inicio</title>
 </head>
 <body>
-    <h1>Hola mundo </h1>
+    <div class="container">
+        <h1 class="text-center">Actividades A y B</h1>
 
+        <div class="btn-group">
+            <a href="ActividadA.php" class="btn btn-primary">Actividad A</a>
+            <div class="col-2"></div>
+            <a href="ActividadB.php" class="btn btn-primary">Actividad B</a>
+        </div>
+    </div>
 
-
-    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
